e17a9957e65a8092e0e268d3a9c8d6e27ef4fb1f
kerlonfernandes <kerlon1221@gmail.com>
2024-05-29 09:01:19 -0300

    tab close button

--- notebook.lua
+++ notebook.lua
@@ -32,6 +32,27 @@ function Notebook.new(TextEditor)
 	self.newTabButton.on_clicked = (function()
 		local te = TextEditor.new()
 
+		local tab_label_box = Gtk.Box({
+			visible = true,
+			spacing = 6,
+			orientation = Gtk.Orientation.HORIZONTAL
+		})
+
+		local tab_label = Gtk.Label({ visible = true, label = "Tab"})
+		local close_button = Gtk.Button({
+			visible = true,
+			relief = Gtk.ReliefStyle.NONE,
+			Gtk.Image({ visible = true, icon_name = "window-close-symbolic"})
+		})
+
+		close_button.on_clicked = function ()
+			local page_num = notebook_obj.notebook:get_current_page()
+			notebook_obj.notebook:remove_page(page_num)			
+		end
+
+		tab_label_box:pack_start(tab_label, true, true, 0)
+		tab_label_box:pack_start(close_button, false, false, 0)
+
 		notebook_obj.editor_tab = te.text_editor
 		notebook_obj.te = te
 
@@ -39,7 +60,9 @@ function Notebook.new(TextEditor)
 			-- Page content
 			notebook_obj.editor_tab,
 			-- Page tab widget
-			Gtk.Label({ visible = true, label = "Tab"})
+			
+			-- Gtk.Label({ visible = true, label = "Tab"}),
+			tab_label_box
 		)
 	end)
 
